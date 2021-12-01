6f83f1c771d267d291eba98af7fea7c8d8aaef6b
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-12-01 16:01:09 +0800

    Code for greetings, and for user input.

---
+++ Sentence-Evaluator.py
@@ -0,0 +1,8 @@
+# Create a program that ask for a sentence as input.
+# Display the number of words, vowels and consonants in the input 
+
+# greetings to the user
+print ("\nWelcome! This program will count the number of words, vowels, and consonants of your input.")
+
+# ask for input
+userInput = input("\nEnter you text here: ")
