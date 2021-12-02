d445dda85a929cb3f77663cdc9a3cb9e06c12a01
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-12-02 18:37:00 +0800

    Changed the text color for convenience.

--- Password-Validator.py
+++ Password-Validator.py
@@ -4,7 +4,7 @@
 # a. Greater than 15 letters
 # b. Have at least one capital letter
 # c. Have at least one number
-# d. Have at least one special char (!@#$%^&*()_+ etc) (~`!@#$%^&*()-_+={}[]|/\:;"'<>.,?)
+# d. Have at least one special char (~`!@#$%^&*()-_+={}[]|/\:;"'<>.,?)
 # ex.
 # input: P@ssw0rd+P@ssw0rd
 # ouput: Valid
@@ -12,10 +12,10 @@
 # Tip: loop through each character of the input then process it letter by letter
 
 # greetings to the user
-print ("\nWELCOME TO JELISHA'S PASSWORD VALIDATOR \n\nThe password is valid if all these criteria are met: \nA. Greater than 15 letters \nB. Have at least one capital letter \nC. Have at least one number \nD. Have at least one special character (!@#$%^&*()_+ etc)")
+print ("\n\033[1mWELCOME TO JELISHA'S PASSWORD VALIDATOR\033[0m \n\nThe \033[92mpassword is valid\033[0m if all these criteria are met: \nA. Greater than \033[4m15 letters\033[0m \nB. Have at least \033[4mone capital letter\033[0m \nC. Have at least \033[4mone number\033[0m \nD. Have at least \033[4mone special character\033[0m (`~!@#$%^&*()-_=+[]|;:,<.>/?)")
 
 # ask for input
-userPass = input("\nEnter your password here:\n")
+userPass = input("\n\033[96mEnter your password here:\033[0m\n")
 
 character = 0
 upper = 0
@@ -29,7 +29,7 @@ valid2 = False
 while not valid1:
     while len(userPass) <= 15:
         if len(userPass) <= 15:
-            userPass = input("\nYour password must be greater than 15 characters. Please try again. \nEnter your password here: ")
+            userPass = input("\nYour password must be \033[91mgreater than 15 characters.\033[0m Please try again. \n\n\033[96mEnter your password here:\033[0m\n")
             if len(userPass) > 15:
                 break
     while not valid2:
@@ -55,16 +55,19 @@ while not valid1:
             else:
                 continue
         if upper > 0 and number > 0 and specialCha > 0:
-            valid = True
+            valid1 = True
             break
         else:
-            valPass = True
-    if valid:
+            valid2 = True
+    if valid1:
         break
-    elif valPass:
-        userPass = input("\nYour password is invalid. It must contain at least one uppercase letter, one number, one special character, and must be greater than 15 characters. Please try again. \nEnter your password here:")
+    elif valid2:
+        userPass = input("\n\033[1m\033[91mYour password is invalid.\033[0m It must contain at least \033[4mone uppercase letter\033[0m, \033[4mone number\033[0m, and \033[4mone special character\033[0m. Please try again. \n\n\033[96mEnter your password here:\033[0m\n")
         character = 0
         upper = 0
         number = 0
         specialCha = 0
-        valPass = False
+        valid2 = False
+
+if valid1:
+    print ("\n\033[1m\033[92mYour password is valid.\033[0m\n")
